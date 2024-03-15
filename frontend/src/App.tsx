98b1ec91f2f71911864e470eea0b385f035bcb15
lorisohr <lyrigs@gmail.com>
2024-03-15 13:30:25 +0100

    created branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Login from "./Login";
 
 const App = () => {
   return (
     <div className="h-34 w-17 bg-red-400">
       <h1>App</h1>
+      <Login />
     </div>
   );
 };
